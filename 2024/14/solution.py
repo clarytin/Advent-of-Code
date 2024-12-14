5d354e12a900c8fcd3a3dd127eb022b0326af1de
Clarissa Sandejas <clarissasandejas@gmail.com>
2024-12-14 18:47:45 +0800

    2024, day 14 - part 2

--- 2024/14/solution.py
+++ 2024/14/solution.py
@@ -2,31 +2,68 @@ import re
 
 
 def main():
-    quads = [0, 0, 0, 0, 0]
     robots = getRobots()
     xs, ys = 101, 103
 
-    for robot in robots:
-        x, y, i = getPosition(xs, ys, 100, robot)
-        quads[i] += 1
+    print(part1(xs, ys, robots))
+    print(part2(xs, ys, robots))
 
-    print(quads[1] * quads[2] * quads[3] * quads[4])
+
+def part1(xs, ys, robots):
+    quads = [0, 0, 0, 0]
+    midx = xs // 2
+    midy = ys // 2
+
+    for robot in robots:
+        x, y = getPosition(xs, ys, 100, robot)
+        
+        if x < midx and y < midy: quads[0] += 1
+        if x < midx and y > midy: quads[1] += 1
+        if x > midx and y < midy: quads[2] += 1
+        if x > midx and y > midy: quads[3] += 1
+
+    return(quads[0] * quads[1] * quads[2] * quads[3])
+
+
+def part2(xs, ys, robots):
+    for seconds in range(xs * ys):
+        locs, visited = set(), set()
+
+        for robot in robots:
+            locs.add(getPosition(xs, ys, seconds, robot))
+
+        for x, y in locs:
+            if bfs(x, y, locs, visited) > 15:
+                return seconds
+
+
+def bfs(x, y, locs, visited):
+    connected = 0
+    q = [(x, y)]
+
+    while q:
+        x, y = q.pop()
+        if (x, y) in visited:
+            continue
+
+        connected += 1
+        visited.add((x, y))
+
+        if (x+1, y) in locs: q.append((x+1, y))
+        if (x-1, y) in locs: q.append((x-1, y))
+        if (x, y+1) in locs: q.append((x, y+1))
+        if (x, y-1) in locs: q.append((x, y-1))
+
+    return connected
 
 
 def getPosition(xs, ys, seconds, robot):
     x, y, vx, vy = robot
-    midx = xs // 2
-    midy = ys // 2
 
     x = (x + (seconds * vx)) % xs
     y = (y + (seconds * vy)) % ys
 
-    if x < midx and y < midy: return x, y, 1
-    if x < midx and y > midy: return x, y, 2
-    if x > midx and y < midy: return x, y, 3
-    if x > midx and y > midy: return x, y, 4
-
-    return x, y, 0
+    return x, y
 
 
 def getRobots():
