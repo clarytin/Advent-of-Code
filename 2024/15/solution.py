958fcc2d2127a8a0cebebbe6db8a98828739d0cc
Clarissa Sandejas <clarissasandejas@gmail.com>
2024-12-15 18:37:14 +0800

    2024, day 15 - part 2

--- 2024/15/solution.py
+++ 2024/15/solution.py
@@ -1,25 +1,22 @@
 def main():
     wh, moves = getInput()
+    wideWh = widen(wh)
 
     r, c = getRobotCoords(wh)
     for move in moves:
         r, c = moveRobot(wh, r, c, move)
+    printGps(wh, "O")
 
-    gps = 0
-    for r in range(len(wh)):
-        for c in range(len(wh[0])):
-            if wh[r][c] == "O": 
-                gps += (100 * r) + c
-
-    print(gps)
+    r, c = getRobotCoords(wideWh)
+    for move in moves:
+        if move == "<" or move == ">":
+            r, c = moveRobot(wideWh, r, c, move)
+        else:
+            r, c = moveSpecial(wideWh, r, c, move)
+    printGps(wideWh, "[")
 
 
 def moveRobot(wh, r, c, m):
-    def move():
-        wh[nR][nC] = wh[r][c]
-        wh[r][c] = "."
-        return nR, nC
-
     moves = {"^": (-1, 0),
              "v": (1,  0),
              "<": (0, -1),
@@ -30,22 +27,97 @@ def moveRobot(wh, r, c, m):
 
     if wh[nR][nC] == "#":
         return r, c
-    
     elif wh[nR][nC] == ".":
-        return move()
-    
+        return move(wh, nR, nC, r, c)
     else:
         boxR, boxC = moveRobot(wh, nR, nC, m)
         if boxR == nR and boxC == nC:
             return r, c
-        return move()
+        return move(wh, nR, nC, r, c)
 
+
+def moveSpecial(wh, r, c, m):
+    nR = r + (-1 if m == "^" else 1)
+
+    if wh[nR][c] == "#":
+        return r, c
+    elif wh[nR][c] == ".":
+        return move(wh, nR, c, r, c)
+    
+    left = c if wh[nR][c] == "[" else c-1
+    if canMove(wh, nR, left, m):
+        moveNextBox(wh, nR, left, m)
+        return move(wh, nR, c, r, c)
+    
+    return r, c
+
+
+def moveNextBox(wh, r, c, m):
+    nR = r + (-1 if m == "^" else 1)
+    
+    if wh[nR][c] == "]":
+        moveNextBox(wh, nR, c-1, m)
+    if wh[nR][c] == "[":
+        moveNextBox(wh, nR, c, m)
+    if wh[nR][c+1] == "[":
+        moveNextBox(wh, nR, c+1, m)
+
+    move(wh, nR, c, r, c)
+    move(wh, nR, c+1, r, c+1)
+
+
+def canMove(wh, r, c, m):
+    nR = r + (-1 if m == "^" else 1) 
+
+    if wh[nR][c] == "#" or wh[nR][c+1] == "#":
+        return False
+    
+    can = True
+    if wh[nR][c] == "]":
+        can = can and canMove(wh, nR, c-1, m)
+    if wh[nR][c] == "[":
+        can = can and canMove(wh, nR, c, m)
+    if wh[nR][c+1] == "[":
+        can = can and canMove(wh, nR, c+1, m)
+        
+    return can
+
+
+def move(wh, nR, nC, r, c):
+    wh[nR][nC] = wh[r][c]
+    wh[r][c] = "."
+    return nR, nC
+
+
+def widen(wh):
+    wides = {"#": ["#", "#"],
+             "O": ["[", "]"],
+             ".": [".", "."],
+             "@": ["@", "."]}
+    new = []
+    for row in wh:
+        newRow = []
+        for ch in row:
+            newRow += wides[ch]
+        new.append(newRow)
+    return new
+        
         
 def getRobotCoords(wh):
     for r in range(len(wh)):
         for c in range(len(wh[0])):
             if wh[r][c] == '@':
                 return r, c
+            
+        
+def printGps(wh, ch):
+    gps = 0
+    for r in range(len(wh)):
+        for c in range(len(wh[0])):
+            if wh[r][c] == ch: 
+                gps += (100 * r) + c
+    print(gps)
+
 
 def getInput():
     f= open("input.txt", "r")
@@ -58,7 +130,7 @@ def getInput():
             warehouse.append([i for i in line[:-1]])
         else:
             moves += [i for i in line[:-1]]
-        if line.find("########") != -1:
+        if line.find("#######") != -1:
             border += 1
 
     f.close()
