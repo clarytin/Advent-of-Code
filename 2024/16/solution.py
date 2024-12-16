4de51976c70d08d62149f036d15c39566488e46a
Clarissa Sandejas <clarissasandejas@gmail.com>
2024-12-16 17:44:32 +0800

    2024, day 16 - part 2

--- 2024/16/solution.py
+++ 2024/16/solution.py
@@ -1,34 +1,40 @@
 def main():
     maze = getInput()
-    minimum = bfs(maze, len(maze) - 2, 1, 1)
+    minimum, bestSeats = bfs(maze, len(maze) - 2, 1, 1)
     print(minimum)
+    print(len(bestSeats))
 
 
 def bfs(maze, r, c, d):
-    dirs = [(-1,0), (0, 1), (1, 0), (0, -1)]
+    ds = [(-1,0), (0, 1), (1, 0), (0, -1)]
     minimum = float("inf")
+    best = set()
     visited = {}
-    q = [(r, c, d, 0)]
+    q = [(r, c, d, 0, set())]
 
     while q:
-        r, c, d, score = q.pop(0) 
+        r, c, d, score, seats = q.pop(0) 
+        seats.add((r, c))
+
         if maze[r][c] == '#':
             continue
         if maze[r][c] == 'E':
-            minimum = min(minimum, score)
+            if score < minimum:
+                best, minimum = seats, score
+            elif score == minimum:
+                best |= seats
             continue
-        if (r, c, d) in visited:
-            if visited[(r, c, d)] <= score:
-                continue
+        if (r, c, d) in visited and visited[(r, c, d)] < score:
+            continue
 
         visited[(r, c, d)] = score
-        q.append((r + dirs[d][0], c + dirs[d][1], d, score + 1))
+        q.append((r + ds[d][0], c + ds[d][1], d, score + 1, seats.copy()))
         d1 = (d - 1) % 4
-        q.append((r + dirs[d1][0], c + dirs[d1][1], d1, score + 1001))
+        q.append((r + ds[d1][0], c + ds[d1][1], d1, score + 1001, seats.copy()))
         d2 = (d + 1) % 4
-        q.append((r + dirs[d2][0], c + dirs[d2][1], d2, score + 1001))
+        q.append((r + ds[d2][0], c + ds[d2][1], d2, score + 1001, seats.copy()))
 
-    return minimum
+    return minimum, best
 
 
 def getInput():
