f5939c488262a4089dfca79a82530c2f8540f33f
Clarissa Sandejas <clarissasandejas@gmail.com>
2024-12-23 02:06:21 +0800

    2024, day 22 - part 2

--- 2024/22/solution.py
+++ 2024/22/solution.py
@@ -1,11 +1,47 @@
 def main():
     inputs = getInputs()
+
     ans = 0
     for num in inputs:
         for _ in range(2000):
             num = getNextNumber(num)
         ans += num
     print(ans)
+
+    d = getSequences(inputs)
+    maximum = 0
+    for prices in d.values():        
+        bananas = sum(prices.values())
+        maximum = max(bananas, maximum)
+        
+    print(maximum)
+
+
+def getSequences(inputs):
+    d = {}
+
+    for num in inputs:
+        curr = num
+        
+        prices = [curr % 10]
+        for _ in range(2000):
+            curr = getNextNumber(curr)
+            prices.append(curr % 10)
+
+        changes = []
+        for i in range(1, len(prices)):
+            changes.append(prices[i] - prices[i-1])
+
+        for i in range(4, len(changes) + 1):
+            seq = tuple(changes[i-4:i])
+            result = prices[i]
+
+            if seq not in d: 
+                d[seq] = {}
+            if num not in d[seq]: 
+                d[seq][num] = result
+
+    return d  
 
 
 def getNextNumber(num):
