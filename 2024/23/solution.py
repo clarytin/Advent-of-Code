1f892dd84ccad25adcdda60364ae8595bfd0d266
Clarissa Sandejas <clarissasandejas@gmail.com>
2024-12-23 23:41:52 +0800

    2024, day 23 - part 2

--- 2024/23/solution.py
+++ 2024/23/solution.py
@@ -2,19 +2,56 @@ import collections
 
 
 def main():
-    parties = set()
     graph = getInput()
+    triplets = getTriplets(graph)
+
+    ans = 0
+    for i in triplets:
+        if i[0][0] == "t" or i[1][0] == "t" or i[2][0] == "t":
+            ans += 1
+
+    print(ans)
+
+    cliques = set()
+
+    maximum = 0
+    largest = None
+    for clique in cliques:
+        largest = clique if len(clique) > maximum else largest
+        maximum = max(maximum, len(clique))
+ 
+    string = str(sorted(list(largest)))[1:-1]
+    print(string.replace(" ", "").replace("\'", ""))
+
+
+def BronKerbosch(R, P, X, graph, cliques):
+    if len(P) == 0 and len(X) == 0:
+        cliques.add(tuple(R))
+        return
+    for v in set(P):
+        BronKerbosch(R | {v}, P & graph[v], X & graph[v], graph, cliques)
+        P -= {v}
+        X | {v} 
+
+
+def connected(comp, key, graph):
+    for c in comp:
+        if c not in graph[key]:
+            return False
+    return True
+  
     
+def getTriplets(graph):
+    parties = set()
+
     for key, val in graph.items():
-        if key[0] != "t":
-            continue
         cpus = list(val)
         for i in range(len(cpus)):
             for j in range(i+1, len(cpus)):
                 if cpus[i] in graph[cpus[j]]:
                     parties.add(tuple(sorted([key, cpus[i], cpus[j]])))
     
-    print(len(parties))
+    return parties
 
 
 def getInput():
